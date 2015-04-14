a5b937100d85b2c009952a362d7054d6a1abeddb
Marcio Marchini <mqm@sglebs.com>
2015-04-14 09:32:02 -0300

    with JavaDoc

--- src/net/betterdeveloper/fitnesse/ExecFixture.java
+++ src/net/betterdeveloper/fitnesse/ExecFixture.java
@@ -4,14 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Slim fixture to execute Runtime.exec commands, see README.md for more information.
+ */
 public class ExecFixture {
 
     private int _lastReturnCode;
 
+    /**
+     * The return code for the executed command
+     */
     public int lastExecReturnCode() {
         return _lastReturnCode;
     }
 
+    /**
+     * Executes a command.
+     *
+     * @param cmd The command to be passed to Runtime.exec
+     * @return The executed process' stdout output
+     */
     public String exec (String cmd) throws IOException, InterruptedException {
         Process p = Runtime.getRuntime().exec (cmd);
         BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
